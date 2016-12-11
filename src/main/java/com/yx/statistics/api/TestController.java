d04c0bb72e8e025b0e9fa3da4eb47e9949bffa1c
ZhangJuntao <369090720@qq.com>
2016-12-11 18:46:23 +0800

    修改注解

--- src/main/java/com/yx/statistics/api/TestController.java
+++ src/main/java/com/yx/statistics/api/TestController.java
@@ -4,10 +4,7 @@ import com.yx.statistics.model.TestPOJO;
 import com.yx.statistics.service.ITestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +13,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/")
+@SuppressWarnings("all")
 public class TestController {
 
     @Autowired
@@ -28,7 +26,7 @@ public class TestController {
     }
 
 
-    @RequestMapping(method = RequestMethod.GET)
+    @PostMapping //@GetMapping
     public List<TestPOJO> index(@RequestParam(value = "age",required = true)Integer age){
         Assert.notNull(age,"age is empty");
         return testService.get(age);
